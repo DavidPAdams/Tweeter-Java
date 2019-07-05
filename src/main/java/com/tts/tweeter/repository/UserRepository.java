99e7485658cf3dc9da1a1210d7567605c574a544
David Adams <david.riding.rails@gmail.com>
2019-07-04 20:00:08 -0700

    add UserRepository

---
+++ src/main/java/com/tts/tweeter/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tts.tweeter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tts.tweeter.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  public User findByUsername(String username);
+}
