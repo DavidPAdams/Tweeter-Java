7048107d9acbf9125eafdd6ddf6b54e21a154fd0
David Adams <david.riding.rails@gmail.com>
2019-07-04 20:21:32 -0700

    modify UserRepository method signature list

--- src/main/java/com/tts/tweeter/repository/UserRepository.java
+++ src/main/java/com/tts/tweeter/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.tts.tweeter.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.tts.tweeter.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   public User findByUsername(String username);
+  public List<User> findAllUsers();
 }
